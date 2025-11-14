4738db4b83eeffd02278a3b5462d5cadf5798e63
Afasarya <coderea9@gmail.com>
2025-11-15 05:44:51 +0700

    Add Product Categories & Products dashboard with full CRUD and image upload

--- src/product-categories/product-categories.module.ts
+++ src/product-categories/product-categories.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductCategoriesService } from './product-categories.service';
 import { ProductCategoriesController } from './product-categories.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadModule],
   controllers: [ProductCategoriesController],
   providers: [ProductCategoriesService],
   exports: [ProductCategoriesService],
